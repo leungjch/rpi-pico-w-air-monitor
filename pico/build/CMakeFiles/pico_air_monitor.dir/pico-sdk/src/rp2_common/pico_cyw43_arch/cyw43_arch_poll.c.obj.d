b860bf85e6c9acfe3b884f49f5d30e795b7c8287
leungjch <justin.leung.ch@gmail.com>
2023-07-12 20:17:51 -0700

    Update gitignore

--- pico/build/CMakeFiles/pico_air_monitor.dir/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/pico_air_monitor.dir/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj: \
- /home/leungjch/Documents/repo/pi-pico-air-monitoring/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
